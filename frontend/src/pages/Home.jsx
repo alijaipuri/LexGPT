4aacbb5fa2dd0bba27cd4791b46a79607f183d05
alijaipuri <138779943+alijaipuri@users.noreply.github.com>
2026-03-10 20:12:34 +0530

    Create Home component with Sidebar and ChatBox

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import ChatBox from "../components/ChatBox"
+import Sidebar from "../components/Sidebar"
+
+export default function Home(){
+
+return(
+
+<div className="flex">
+
+<Sidebar/>
+
+<ChatBox/>
+
+</div>
+
+)
+
+}
